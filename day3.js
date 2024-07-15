348d6e3a8710ad06f783ccf4c935b0d8737dcb18
Saifullah <saifu3140@example.com>
2024-07-15 23:31:42 +0530

    Day 3

--- day3.js
+++ day3.js
@@ -10,3 +10,32 @@ else if(num>0){
 else{
     console.log("negative")
 }
+
+// Activity 2:
+const a = 4
+const b = 1
+const c = 9
+if(a>b){
+    if(a>c){
+        console.log("Largest is: ",c)
+    }
+}
+
+// Activity 4:
+const ch = 7
+switch(ch){
+    case 1:
+        console.log("Mon")
+        break;
+    case 2:
+        console.log("Tues")
+        break;
+    case 3:
+        console.log("Wed")
+        break;
+    case 4:
+        console.log("Thur")
+        break;
+    default:
+        console.log("Friday")
+}
